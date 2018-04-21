53c85d3a6f3ab9498d2e7e23168d902373273282
Tommy Ready <tommy.r.ready@gmail.com>
2018-04-21 03:47:44 -0400

    Update UsersTableSeeder.php
    Remove name field from users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-        	'name' => 'John Doe',
         	'email' => 'john.doe@example.com',
         	'password' => bcrypt('321ewq'),
         ]);
